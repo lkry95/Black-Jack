d47a5cdaed0fd790c43b87d7290f8d5ba1ef5065
Luke Roy <lkry95@gmail.com>
2020-12-03 16:52:51 -0500

    Add files via upload

---
+++ black_jack.py
@@ -0,0 +1,46 @@
+
+
+class Deck:
+
+    def __init__(self):
+        pass
+
+
+
+
+
+
+class Deal:
+
+    def __init__(self, deck):
+        self.deck = deck
+
+
+
+
+
+class Calculation:
+
+    def __init__(self):
+        pass
+
+
+
+
+class Player:
+
+    def __init__(self):
+        pass
+
+    def player_hit(self, my_cards, deck):
+
+
+
+class Dealer(Player):
+
+    def __init__(self):
+        pass
+
+
+
+
